655bf960662f9bceb9415a66d5ff7be26e971ba5
Elio Hoyos <elio18david@gmail.com>
2023-03-14 10:32:05 -0500

    switch

---
+++ SwitchClase/SwitchClase/view/PrimeraPaginaView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SwitchClase.view
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PrimeraPaginaView : ContentPage
+    {
+        public PrimeraPaginaView()
+        {
+            InitializeComponent();
+        }
+
+        public async void Button_Clicked(object sender, EventArgs e) { 
+            
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            string dni = txtDNI.Text;
+            string direccion = txtDir.Text;
+            string celular = txtCel.Text;
+            await Application.Current.MainPage.Navigation.PushModalAsync(new SegundaPaginaView(nombre, apellido,
+                dni, direccion, celular));
+        
+        }
+
+    }
+}
---
+++ SwitchClase/SwitchClase/view/SegundaPaginaView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SwitchClase.view
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SegundaPaginaView : ContentPage
+    {
+        public SegundaPaginaView(string nombre, string apellido, string dni, string direccion, string celular)
+        {
+            InitializeComponent();
+            LabelNombre.Text = "Hola Señor " + nombre + " " + apellido;
+            LabelDNI.Text = "Mi DNI es : " + dni;
+            LabelDireccion.Text = "Mi Dirección es : " + direccion;
+            LabelCelular.Text = "Mi número de celular es : " + celular;
+
+        }
+    }
+}
